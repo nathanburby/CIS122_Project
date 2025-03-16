9d7504450f7d9e15c261fb35544b20fbe83adba7
nathanburby <wi3741jj@go.minnstate.edu>
2025-03-15 20:56:06 -0500

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS122_Project
+{
+	public class Book
+	{
+		public string _title;
+		public string _author;
+		public string _isbn;
+		public int _publicationDate;
+
+		public Book(string title, string isbn) : this(title, string.Empty, isbn, -1)
+		{
+			this._title = title;
+			this._isbn = isbn;
+		}
+
+		public Book(string title, string author, string isbn, int publicationDate)
+		{
+			this._title = title;
+			this._author = author;
+			this._isbn = isbn;
+			this._publicationDate = publicationDate;
+		}
+
+		public Book() : this("N/a", "N/a", "N/a", -1) { }
+
+
+	}
+}
---
+++ Library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS122_Project
+{
+	public class Library
+	{
+		public List<Book> _books;
+
+		public Library()
+		{
+			_books = new List<Book>();
+		}
+
+		public void AddBook(Book newBook)
+		{
+			_books.Add(newBook);
+		}
+
+		public bool DeleteBook(Book findBook)
+		{
+			bool removed = false;
+			for (int i = _books.Count - 1; i >= 0; i--)
+			{
+				if (_books[i] == findBook)
+				{
+					_books.RemoveAt(i);
+					removed = true;
+					break;
+				}
+			}
+			return removed;
+		}
+
+		public bool Replace(Book oldBook, Book newBook)
+		{
+			if (newBook == null || oldBook == newBook) return false;
+
+			int index = _books.IndexOf(oldBook);
+			if (index != -1)
+			{
+				_books[index] = newBook;
+				return true;
+			}
+			return false;
+		}
+	}
+}
